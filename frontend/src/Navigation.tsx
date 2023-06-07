88a27fa3ec236b8aafa3f96067c94ea3ebdb1dbb
Luong Khiem Tran <khiem.tran@idt.net>
2023-06-08 02:11:22 +0300

    chore: update

--- frontend/src/Navigation.tsx
+++ frontend/src/Navigation.tsx
@@ -98,11 +98,10 @@ export default function Navigation({
   return (
     <>
       <div
-        style={{ width: '32rem' }}
         ref={navRef}
         className={`${
           navState === 'INACTIVE' && '-ml-96 md:-ml-[14rem]'
-        } duration-20 fixed z-20 flex h-full flex-col border-r-2 bg-gray-50 transition-all`}
+        } duration-20 fixed z-20 flex h-full flex-col border-r-2 w-96 bg-gray-50 transition-all`}
       >
         <div className={'visible h-16 w-full border-b-2 md:hidden'}>
           <button
